d8a5749e7c32a87647b0076d58437f575a1dddc5
Bernardo Silva <contato@bernardosilva.com.br>
2021-02-17 14:58:30 -0300

    correção do callback

--- lib.php
+++ lib.php
@@ -1,9 +1,9 @@
 <?php
 
-function optimizer_after_file_created($record){
+function local_optimizer_after_file_created($record){
     error_log("created: ".var_export($record, true));
 }
 
-function optimizer_after_file_deleted($record){
+function local_optimizer_after_file_deleted($record){
     error_log("deleted: ".var_export($record, true));
 }
--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2021021701;
+$plugin->version = 2021021702;
 $plugin->requires = 2016052300;
 $plugin->component = 'local_optimizer';
 $plugin->release = '3';
